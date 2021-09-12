9a05e9fd34a446b1789ef3d033c2f579554849c0
mdmaruf641 <mdmarufmoni50@gmail.com>
2021-09-13 03:23:30 +0600

    bugs fix

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ const showProducts = (products) => {
       <p>Category: ${product.category}</p>
       
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-info">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
